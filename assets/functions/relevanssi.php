50d95c53efb195044a07cb5b34213f8064910cb2
Willem Prins | SOMTIJDS <willem@somtijds.nl>
2017-01-04 15:10:48 +0100

    Add postmeta to filter

--- assets/functions/relevanssi.php
+++ assets/functions/relevanssi.php
@@ -11,33 +11,47 @@
 
 add_filter('relevanssi_content_to_index', 'exchange_relevanssi_add_extra_content', 10, 2);
 function exchange_relevanssi_add_extra_content($content, $post) {
-    if ( 'collaboration' !== get_post_type( $post ) ) {
-    	return $content;
-    }
-    $collab = BaseController::exchange_factory( $post );
-    if ( ! $collab instanceof Collaboration ) {
-    	return $content;
-    }
-    $collab->controller->set_participants();
-    if ( empty( $collab->participants ) ) {
-    	return $content;
-    }
-    $add_to_content = array();
-    $index_these_props = array('name','org_name','org_description','org_country');
-    foreach( $collab->participants as $participant ) {
-    	$vars = get_object_vars( $participant );
-    	if ( empty( $vars ) ) {
-    		continue;
-    	}
-    	foreach( $index_these_props as $prop ) {
-    		if ( ! empty( $participant->$prop ) ) {
-    			$add_to_content[] = $participant->$prop;
-    		}
-    	}
+	if ( $post->post_status !== 'publish' ) {
+		return $content;
+	}
+	$add_to_content = array();
+    if ( 'collaboration' === $post->post_type ) {
+	    $collab = BaseController::exchange_factory( $post );
+	    if ( ! $collab instanceof Collaboration ) {
+	    	return $content;
+	    }
+	    $collab->controller->set_participants();
+	    if ( empty( $collab->participants ) ) {
+	    	return $content;
+	    }
+	    $add_to_content = array();
+	    $index_these_props = array('name','org_name','org_description','org_country');
+	    foreach( $collab->participants as $participant ) {
+	    	$vars = get_object_vars( $participant );
+	    	if ( empty( $vars ) ) {
+	    		continue;
+	    	}
+	    	foreach( $index_these_props as $prop ) {
+	    		if ( ! empty( $participant->$prop ) ) {
+	    			$add_to_content[] = $participant->$prop;
+	    		}
+	    	}
+	    }
+    } elseif ( 'story' === $post->post_type ) {
+    	global $wpdb;
+    	$rows = $wpdb->get_results("SELECT meta_value FROM wp_postmeta WHERE post_id = $post->ID AND meta_key LIKE 'sections_%text' OR meta_key LIKE 'sections_%question' OR meta_key LIKE 'sections_%answer'");
+    	if ( empty( $rows ) ) {
+    		return $content;
+		}
+		foreach( $rows as $row ) {
+			if ( $row->meta_value !== null ) {
+				$add_to_content[] = $row->meta_value;
+			}
+		}
     }
     if ( empty( $add_to_content ) ) {
-    	return $content;
-    }
-    $content .= implode(' | ', $add_to_content );
-    return $content;
+		return $content;
+	}
+	$content .= implode(' | ', $add_to_content );
+	return $content;
 }
